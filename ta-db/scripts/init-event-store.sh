74436d78238c0b096d75cc6721f5e11f2d336fd4
Gabriel Schenker <gnschenker@gmail.com>
2022-04-09 22:23:15 +0200

    Added PostgeSQL support

---
+++ ta-db/scripts/init-event-store.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE TABLE events(
+        index serial not null PRIMARY KEY,
+        aggregateid integer not null,
+        version integer not null,
+        body varchar(2000) not null,
+        metadata varchar(2000),
+        dispatched boolean not null default false
+    );
+
+    CREATE INDEX index_events_aggregateid ON events(aggregateid);
+    CREATE INDEX index_events_aggregateid_version ON events(aggregateid, version);
+    CREATE INDEX index_events_dispatched ON events(dispatched);
+EOSQL
